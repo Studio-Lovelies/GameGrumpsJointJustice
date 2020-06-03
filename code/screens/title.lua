f94520a448459e7da168e2a1065e68eb07823390
IsaacLaquerre <killers3d@gmail.com>
2020-06-03 00:01:42 -0400

    Made menu freely navigatable

--- code/screens/title.lua
+++ code/screens/title.lua
@@ -86,42 +86,169 @@ function DrawTitleScreen()
     )
 end
 
+titleSelections = {}
+titleSelections[0] = "New Game";
+titleSelections[1] = "Browse Scenes";
+titleSelections[2] = "Load Game";
+SelectionIndex = 0;
+
 TitleScreenConfig = {
     displayed = false;
-    onKeyPressed = function (key)
+    onKeyPressed = function(key)
         if key == controls.start_button then
             -- Since there's no displayKey, this screen
             -- is responsible for removing itself
             screens.title.displayed = false;
             love.graphics.clear(0,0,0);
-            if TitleSelection == "Load Game" then
-                -- replace this and handle load game logic
-                Episode:begin()
-            elseif TitleSelection == "Browse Scenes" then
+            if TitleSelection == "Browse Scenes" then
                 -- browse scenes screen here
-                love.graphics.draw(
-                    blackImage,
-                    GetCenterOffset(blackImage:getWidth() * blackScale, false),
-                    0,
-                    0,
-                    blackScale,
-                    blackScale
-                )
-            else
+                screens.browsescenes.displayed = true;
+                DrawBrowseScreen()
+            --elseif TitleSelection == "Load Game" then
+                -- replace this and handle load game logic
+            --    Episode:begin()
+            --elseif TitleSelection == "New Game" then
                 -- replace this and handle new game logic
-                Episode:begin()
+            --    Episode:begin()
             end
         elseif key == controls.press_right then
-            TitleSelection = "Load Game"
+            SelectionIndex = SelectionIndex + 1
+            if (SelectionIndex > 2) then
+                SelectionIndex = 0
+            end
+            TitleSelection = titleSelections[SelectionIndex]
         elseif key == controls.press_left then
-            TitleSelection = "New Game"
-        elseif key == controls.pause_nav_down then
-            TitleSelection = "Browse Scenes"
+            SelectionIndex = SelectionIndex - 1
+            if (SelectionIndex < 0) then
+                SelectionIndex = 2
+            end
+            TitleSelection = titleSelections[SelectionIndex]
         end
     end;
     onDisplay = function ()
-        -- This will depend on whether or not there are saves
-        TitleSelection = "New Game"
+        TitleSelection = "New Game";
+    end;
+    draw = function ()
+        DrawTitleScreen()
+    end;
+}function DrawBrowseScreen()
+    local blackImage = love.graphics.newImage(settings.black_screen_path)
+    local blackScale = 1
+
+    love.graphics.clear(unpack(colors.black))
+    love.graphics.draw(
+        blackImage,
+        GetCenterOffset(blackImage:getWidth() * blackScale, false),
+        0,
+        0,
+        blackScale,
+        blackScale
+    )
+
+    local backW = (dimensions.window_width * 1/5)
+    local backX = (dimensions.window_width * 1/18)
+    local backY = blackImage:getHeight()*blackScale
+    local backH = 60
+
+    local pretrialW = (dimensions.window_width * 1/3.75)
+    local pretrialX = (dimensions.window_width * 10.75/18) - pretrialW
+    local pretrialY = blackImage:getHeight()*blackScale
+    local pretrialH = 60
+
+    local jorytrialW = (dimensions.window_width * 1/3.75)
+    local jorytrialX = (dimensions.window_width * 17/18) - jorytrialW
+    local jorytrialY = blackImage:getHeight()*blackScale
+    local jorytrialH = 60
+
+    local dx = 8
+    local dy = 8
+
+    love.graphics.setColor(0.44,0.56,0.89)
+    if TitleSelection == "Back" then
+        love.graphics.rectangle("fill", backX-dx, backY-dy, backW+2*dx, backH+2*dy)
+    elseif TitleSelection == "Jory's Trial" then
+        love.graphics.rectangle("fill", jorytrialX-dx, jorytrialY-dy, jorytrialW+2*dx, jorytrialH+2*dy)
+    else
+        love.graphics.rectangle("fill", pretrialX-dx, pretrialY-dy, pretrialW+2*dx, pretrialH+2*dy)
+    end
+
+    love.graphics.setColor(222, 0, 0)
+    love.graphics.rectangle("fill", backX, backY, backW, backH)
+
+    love.graphics.setColor(0.3,0.3,0.3)
+    love.graphics.rectangle("fill", pretrialX, pretrialY, pretrialW, pretrialH)
+
+    love.graphics.setColor(0.3,0.3,0.3)
+    love.graphics.rectangle("fill", jorytrialX, jorytrialY, jorytrialW, jorytrialH)
+
+    love.graphics.setColor(1,1,1)
+    local textScale = 3
+    local backText = love.graphics.newText(GameFont, "Back")
+    love.graphics.draw(
+        backText,
+        backX + backW/2-(backText:getWidth() * textScale)/2,
+        backY + backH/2-(backText:getHeight() * textScale)/2,
+        0,
+        textScale,
+        textScale
+    )
+
+    local pretrialText = love.graphics.newText(GameFont, "Pre-Trial")
+    love.graphics.draw(
+        pretrialText,
+        pretrialX + pretrialW/2-(pretrialText:getWidth() * textScale)/1.5,
+        pretrialY + pretrialH/2-(pretrialText:getHeight() * textScale)/2,
+        0,
+        textScale,
+        textScale
+    )
+
+    local jorytrialText = love.graphics.newText(GameFont, "Jory's Trial")
+    love.graphics.draw(
+        jorytrialText,
+        jorytrialX + jorytrialW/2-(jorytrialText:getWidth() * textScale)/2,
+        jorytrialY + jorytrialH/2-(jorytrialText:getHeight() * textScale)/2,
+        0,
+        textScale,
+        textScale
+    )
+end
+
+sceneSelections = {}
+sceneSelections[0] = "Back";
+sceneSelections[1] = "Pre-Trial";
+sceneSelections[2] = "Jory's Trial";
+SelectionIndex = 0;
+
+BrowseScreenConfig = {
+    displayed = false;
+    onKeyPressed = function (key)
+        if key == controls.start_button then
+            screens.title.displayed = false;
+            love.graphics.clear(0,0,0);
+            if TitleSelection == "Pre-Trial" then
+                --Episode:begin()
+            elseif TitleSelection == "Jory's Trial" then
+                --NewEpisode(settings.jory_trial_path):begin()
+            elseif TitleSelection == "Back" then
+                DrawTitleScreen()
+            end
+        elseif key == controls.press_right then
+            SelectionIndex = SelectionIndex + 1
+            if (SelectionIndex > 2) then
+                SelectionIndex = 0
+            end
+            TitleSelection = sceneSelections[SelectionIndex]
+        elseif key == controls.press_left then
+            SelectionIndex = SelectionIndex - 1
+            if (SelectionIndex < 0) then
+                SelectionIndex = 2
+            end
+            TitleSelection = sceneSelections[SelectionIndex]
+        end
+    end;
+    onDisplay = function ()
+        TitleSelection = "Back"
     end;
     draw = function ()
         DrawTitleScreen()
