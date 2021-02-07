a4e9397d08917505550a5c277af0451033be020e
IsaacLaquerre <killers3d@gmail.com>
2021-02-07 03:13:26 -0500

    forgot to change this back

--- code/scene.lua
+++ code/scene.lua
@@ -88,7 +88,7 @@ function NewScene(scriptPath)
         if self.credits ~= nil and #self.creditLines > 1 then
             for i = 1, #self.creditLines do
                 if self.creditLines[#self.creditLines][3] > 60 then
-                    self.creditLines[i][3] = self.creditLines[i][3] - 30--0.5
+                    self.creditLines[i][3] = self.creditLines[i][3] - 0.5
                 end
             end
         end
