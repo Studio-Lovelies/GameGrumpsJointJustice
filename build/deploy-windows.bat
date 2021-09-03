eb2f3b4892f2e878d165dd46edd548b9c63be057
notexplosive <thenotexplosive@gmail.com>
2021-09-02 23:27:55 -0700

    Add build and deploy scripts - currently these only work on wyatt's computer

---
+++ build/deploy-windows.bat
@@ -0,0 +1,5 @@
+REM This batch script uploads the directory .\windows to itch.io as a zip under the "windows" channel
+
+REM Currently these only work on Wyatt's personal machine, you'll need to change the API key path to work in your setting
+REM You'll also need Butler installed
+butler -i C:\users\wyatt\.config\itch\studiolovelies_creds push windows studiolovelies/game-grumps-joint-justice-lua:windows
