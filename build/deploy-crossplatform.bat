5b62330396c7cc95a6572fe67d755bec0a9f5b63
notexplosive <thenotexplosive@gmail.com>
2021-09-03 08:55:36 -0700

    Fix URLs in deploy scripts

--- build/deploy-crossplatform.bat
+++ build/deploy-crossplatform.bat
@@ -2,4 +2,4 @@ REM This batch script uploads the file .\GameGrumpsJointJustice.love to itch.io 
 
 REM Currently these only work on Wyatt's personal machine, you'll need to change the API key path to work in your setting
 REM You'll also need Butler installed
-butler -i C:\users\wyatt\.config\itch\studiolovelies_creds push GameGrumpsJointJustice.love studiolovelies/game-grumps-joint-justice-lua:cross-platform
+butler -i C:\users\wyatt\.config\itch\studiolovelies_creds push GameGrumpsJointJustice.love studiolovelies/ggjj-lua:cross-platform
--- build/deploy-windows.bat
+++ build/deploy-windows.bat
@@ -2,4 +2,4 @@ REM This batch script uploads the directory .\windows to itch.io as a zip under 
 
 REM Currently these only work on Wyatt's personal machine, you'll need to change the API key path to work in your setting
 REM You'll also need Butler installed
-butler -i C:\users\wyatt\.config\itch\studiolovelies_creds push windows studiolovelies/game-grumps-joint-justice-lua:windows
+butler -i C:\users\wyatt\.config\itch\studiolovelies_creds push windows studiolovelies/ggjj-lua:windows
